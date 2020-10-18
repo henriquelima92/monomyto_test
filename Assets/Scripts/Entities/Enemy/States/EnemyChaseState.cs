800e3e5e6c08053d8d8fe0658b3765c32c891531
henriquelima92 <henriquedelima92@hotmail.com>
2020-10-18 15:51:45 -0300

    added the enemy state sripts.

---
+++ Assets/Scripts/Entities/Enemy/States/EnemyChaseState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChaseState : State
+{
+    public EnemyChaseState(Entity entity) : base(entity) { }
+    public override void Tick() { }
+    public override void OnStateEnter() { }
+    public override void OnStateExit() { }
+}
---
+++ Assets/Scripts/Entities/Enemy/States/EnemyMovementState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementState : State
+{
+    public EnemyMovementState(Entity entity) : base(entity) { }
+    public override void Tick() { }
+    public override void OnStateEnter() { }
+    public override void OnStateExit() { }
+}
